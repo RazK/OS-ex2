d642a04c4ce1ee9e01e93499e08018b94dc424ac
ROBO-LAPTOP\Raz <Raz@yosigal.com>
2018-04-30 12:00:33 +0300

    Modified CMakeList to compile the demos as well as the tester

--- uthreads.cpp
+++ uthreads.cpp
@@ -4,42 +4,107 @@
 #include "uthreads.h"
 #include <stdio.h>
 #include <cstdlib>
+#include <machine/setjmp.h>
+#include <setjmp.h>
 
-#define RET_ERR             (-1)
-#define RET_SUCCESS         0
+const int RET_ERR = (-1);
+const int RET_SUCCESS = 0;
 
-#define STATE_RUNNING       1
-#define STATE_READY         0
-#define STATE_BLOCKED       2
+const int ID_SCHEDUELER = 0;
+const int ID_FIRST_USER_THREAD = 1;
 
-#define STATUS_ALIVE        true
-#define STATUS_TERMINATED   false
+#ifdef __x86_64__
+/* code for 64 bit Intel arch */
 
+typedef unsigned long address_t;
+#define JB_SP 6
+#define JB_PC 7
 
+/* A translation is required when using an address of a variable.
+   Use this as a black box in your code. */
+address_t translate_address(address_t addr)
+{
+    address_t ret;
+    asm volatile("xor    %%fs:0x30,%0\n"
+            "rol    $0x11,%0\n"
+    : "=g" (ret)
+    : "0" (addr));
+    return ret;
+}
+
+#else
+/* code for 32 bit Intel arch */
+
+typedef unsigned int address_t;
+#define JB_SP 4
+#define JB_PC 5
+
+/* A translation is required when using an address of a variable.
+   Use this as a black box in your code. */
+address_t translate_address(address_t addr)
+{
+    address_t ret;
+    asm volatile("xor    %%gs:0x18,%0\n"
+		"rol    $0x9,%0\n"
+                 : "=g" (ret)
+                 : "0" (addr));
+    return ret;
+}
+#endif
+
+typedef enum _State {
+    READY,
+    RUNNING,
+    BLOCKED,
+    NUM_OF_STATES
+} State;
+
+typedef enum _Status {
+    ALIVE,
+    TERMINATED,
+    NUM_OF_STATUSES
+} Status;
 
 /* Data per thread. */
-typedef struct Thread {
-    // Global thread id
-    int id;
+typedef struct _Thread {
+    address_t sp;   // Stack Pointer: Address of thread's stack head
+    address_t pc;   // Program Counter: Address of thread's current instruction
+    State state;    // Scheduling State: one of [Ready, Running, Blocked]
+    Status status;  // Thread Status: alive or terminated.
 
-    // Schedualing State: one of [Ready, Running, Blocked]
-    int state;
+    _Thread()
+    {
+        sp = NULL;
+        pc = NULL;
+        state = State::READY;
+        status = Status::TERMINATED;
+    }
 
-    void * SP; /* #todo What type should this pointer have? */
-    // Should we save using a pointer to the stack and a positional argument?
-
-    // Thread Status: alive or terminated.
-    bool status;
-
-
-
+    _Thread(address_t sp, address_t pc, State state, Status status)
+    {
+        this->sp = sp;
+        this->pc = pc;
+        this->state = state;
+        this->status = status;
+    }
 } Thread;
+
+/* Scheduler thread stack */
+char stack_scheduler[STACK_SIZE];
+
+/* Scheduler thread function */
+// TODO: Implement real scheduler function
+int scheduler_f(){
+    printf("Hi i'm the scheduler\r\n");
+    return RET_SUCCESS;
+}
 
 /* Global counter for all quantums. */
 static int total_quantums;
 
-/* Data Structure holding all threads. */
+/* Threads descriptor lookup table. */
 Thread thread_list[MAX_THREAD_NUM]; // #todo Should I send MAX_THREAD_NUM-1 (for main thread?)
+sigjmp_buf env[MAX_THREAD_NUM];
 
 int uthread_init(int quantum_usecs){
     if (quantum_usecs < 0){
@@ -48,18 +113,21 @@ int uthread_init(int quantum_usecs){
     total_quantums = 1;
 
     // quantum timer + scheduler
-
-    // init the list of threads
-    // Did not start from 0 here, reserved first thread for main
-    for (int i = 1; i < MAX_THREAD_NUM; i++){
-        thread_list[i] = {i /* id */, STATE_READY /*state*/, nullptr /* SP */, STATUS_TERMINATED
-                /* Status*/ };
+    // TODO: Init scheduler thread
+    auto sp = (address_t)stack_scheduler + STACK_SIZE - sizeof(address_t);
+    auto pc = (address_t)scheduler_f;
+    thread_list[ID_SCHEDUELER] = Thread(sp, pc, State::RUNNING, Status::ALIVE);
+    sigsetjmp(env[ID_SCHEDUELER], 1);
+    (env[0]->__jmpbuf)[JB_SP] = translate_address(sp);
+    (env[0]->__jmpbuf)[JB_PC] = translate_address(pc);
+    sigemptyset(&env[0]->__saved_mask);
+ 
+    // Init user threads
+    for (int thread_id = ID_FIRST_USER_THREAD; thread_id < MAX_THREAD_NUM; thread_id++){
+        thread_list[thread_id] = Thread();
     }
 
-
-
     return RET_SUCCESS;
-
 }
 
 int uthread_spawn(void (*f)()){
@@ -67,8 +135,8 @@ int uthread_spawn(void (*f)()){
 
     // Find the minimal ID not yet taken.
     for (id = 1; id < MAX_THREAD_NUM; id++){
-        if (thread_list[id].status == STATUS_TERMINATED){ // Can be shortened to if(..status)
-            thread_list[id].status = STATUS_ALIVE;
+        if (thread_list[id].status == Status::TERMINATED){ // Can be shortened to if(..status)
+            thread_list[id].status = Status::ALIVE;
             break;
         }
     }
@@ -77,9 +145,9 @@ int uthread_spawn(void (*f)()){
         return RET_ERR; // No more room for threads
     }
 
-    thread_list[id].SP = malloc(STACK_SIZE);
+    thread_list[id].sp = (address_t )malloc(STACK_SIZE);
 
-    if (thread_list[id].SP == NULL){
+    if (thread_list[id].sp == NULL){
         return RET_ERR; // Unsuccessful malloc
     }
 
@@ -90,39 +158,42 @@ int uthread_spawn(void (*f)()){
     return id;
 }
 
+// TODO: Implement
 int uthread_terminate(int tid){
     printf("pass\r\n");
     return RET_SUCCESS;
 }
-
+// TODO: Implement
 int uthread_block(int tid){
     printf("pass\r\n");
     return RET_SUCCESS;
 }
 
+// TODO: Implement
 int uthread_resume(int tid){
     printf("pass\r\n");
     return RET_SUCCESS;
 }
 
+// TODO: Implement
 int uthread_sync(int tid){
     printf("pass\r\n");
     return RET_SUCCESS;
 }
 
+// TODO: Implement
 int uthread_get_tid(){
     printf("pass\r\n");
     return RET_SUCCESS;
-
 }
 
+// TODO: Implement
 int uthread_get_total_quantums(){
     return total_quantums;
-
 }
 
+// TODO: Implement
 int uthread_get_quantums(int tid){
     printf("pass\r\n");
     return RET_SUCCESS;
-
 }
