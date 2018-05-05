3ab74267965b1ac966ccd10ffbf13be6c615a687
Raz <raz@yosigal.com>
2018-05-05 15:35:09 +0300

    CODE COMPILES (Lots of errors are just commented out, work from here)

--- uthreads.cpp
+++ uthreads.cpp
@@ -4,8 +4,8 @@
 #include "uthreads.h"
 #include <stdio.h>
 #include <cstdlib>
-#include <machine/setjmp.h>
 #include <setjmp.h>
+#include <signal.h>
 #include <queue>
 #include <list>
 
@@ -55,7 +55,7 @@ address_t translate_address(address_t addr)
 }
 #endif
 
-typedef const u_int8_t threadID;
+typedef u_int8_t threadID;
 
 typedef enum _State {
     READY,
@@ -72,26 +72,30 @@ typedef enum _Status {
 
 /* Data per thread. */
 typedef struct _Thread {
-    address_t sp;   // Stack Pointer: Address of thread's stack head
-    address_t pc;   // Program Counter: Address of thread's current instruction
+    //address_t sp;   // Stack Pointer: Address of thread's stack head
+    //address_t pc;   // Program Counter: Address of thread's current instruction
     State state;    // Scheduling State: one of [Ready, Running, Blocked]
     Status status;  // Thread Status: alive or terminated.
-    std::queue <threadID> synced_threads{}; // All the threads that called "sync" for this thread.
+    std::queue <threadID> synced_threads; // All the threads that called "sync" for this thread.
+    // TODO: allocate memory in CTOR for this queue
+    // TODO: free queue's memory in DTOR
 
     _Thread()
     {
-        sp = NULL;
-        pc = NULL;
+        //sp = NULL;
+        //pc = NULL;
         state = State::READY;
         status = Status::TERMINATED;
+        // TODO: allocate synced_threads queue
     }
 
     _Thread(address_t sp, address_t pc, State state, Status status)
     {
-        this->sp = sp;
-        this->pc = pc;
+        //this->sp = sp;
+        //this->pc = pc;
         this->state = state;
         this->status = status;
+        // TODO: free synced_threads queue
     }
 } Thread;
 
@@ -106,7 +110,7 @@ typedef enum _MaskingCode {
 // in any given scenario, and call the reciprocal unmask function when the scope is exited.
 typedef struct _Mask{
 
-    void _Mask(MaskingCode code){
+    _Mask(MaskingCode code){
         if (code == SCHEDULER){
             //todo save old mask, and then add appropriate mask settings per scenario (code)
             sigmask(1);
@@ -186,11 +190,11 @@ int uthread_spawn(void (*f)()){
         return RET_ERR; // No more room for threads
     }
 
-    thread_list[id].sp = (address_t )malloc(STACK_SIZE);
+    //thread_list[id].sp = (address_t )malloc(STACK_SIZE);
 
-    if (thread_list[id].sp == NULL){
-        return RET_ERR; // Unsuccessful malloc
-    }
+    //if (thread_list[id].sp == NULL){
+    //    return RET_ERR; // Unsuccessful malloc
+    //}
 
     thread_list[id].status = Status::ALIVE; // Set thread as alive for use
 
@@ -225,6 +229,7 @@ int uthread_terminate(int tid){
     // Iterate the queue of synced threads to allow them to run
     while (!thread_list[tid].synced_threads.empty()){
         threadID synced_thread = thread_list[tid].synced_threads.front();
+        // TODO: RazK: (below) What if this thread was blocking? why did we decide it's READY?
         thread_list[synced_thread].state = READY;
         ready_queue.push(synced_thread);
         // No need to check if synced thread is alive. This is responsibility of scheduler.
@@ -244,6 +249,7 @@ int uthread_block(int tid){
     }
     thread_list[tid].state = State::BLOCKED;
 
+    // TODO: Get the real running_thread
     threadID running_thread = 0;
     if (running_thread == tid){ // thread blocking itself
         //todo scheduling decision
